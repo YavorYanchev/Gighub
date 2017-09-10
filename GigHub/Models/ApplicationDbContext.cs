6a5cb2035427923da8fb02837b923839342067f3
YavorYanchev <yavor_yanchev_bs@abv.bg>
2017-09-10 23:11:35 +0300

    Added name to user and build API for the attendances

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace GigHub.Models
         public DbSet<Gig> Gigs { get; set; }
 
         public DbSet<Genre> Genres { get; set; }
+
+        public DbSet<Attendance> Attendances { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -20,6 +22,16 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //turn off cascade delete with FluentAPI
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
